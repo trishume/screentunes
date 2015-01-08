4830949f13a75a6d4804122508be703c1be25843
Tristan Hume <tris.hume@gmail.com>
2015-01-08 17:49:27 -0500

    Initial commit

---
+++ screentunes.js
@@ -0,0 +1,73 @@
+(function() {
+  var canvas;
+  var ctx;
+  var start = null;
+  var t = null;
+
+  var AnimationFrame = (function() {
+    var FPS = 16.6666666667; // 1000 / 60 = Frames Per Second
+    var RAF = window.requestAnimationFrame
+          || window.webkitRequestAnimationFrame
+          || window.mozRequestAnimationFrame
+          || window.msRequestAnimationFrame
+          || window.oRequestAnimationFrame
+          || function(a) { window.setTimeout(a, FPS); }
+    var CAF = window.cancelAnimationFrame
+          || window.webkitCancelAnimationFrame
+          || window.mozCancelAnimationFrame
+          || window.msCancelAnimationFrame
+          || window.oCancelAnimationFrame
+          || function(a) { window.clearTimeout(a); }
+    return {
+      request: function(a) {
+        RAF(a);
+      },
+      cancel: function(a) {
+        CAF(a);
+      }
+    }
+  })();
+
+  function resize() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    /* $("#canvas").css("width", w + "px");
+     $("#canvas").css("height", h + "px"); */
+    /* render(); */
+  }
+
+  function bar(y, height) {
+    ctx.fillRect(0,y,canvas.width,height);
+  }
+
+  function bars(spacing, height) {
+    ctx.fillStyle = "rgb(0,0,0)";
+    var y = 0;
+    var maxy = canvas.height;
+    while(y < maxy) {
+      bar(y,height);
+      y += spacing + height;
+    }
+  }
+
+  function render() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    var magic = 5 + (Math.sin(t / 2000)+1)*7;
+    bars(magic, magic);
+  }
+
+  function frame(timestamp) {
+    if (start === null) start = timestamp;
+    t = timestamp - start;
+    render();
+    AnimationFrame.request(frame);
+  }
+
+  $(document).ready(function() {
+    canvas = document.getElementById("main");
+    ctx = canvas.getContext("2d");
+    $(window).bind("resize", resize);
+    resize();
+    AnimationFrame.request(frame);
+  });
+})();
