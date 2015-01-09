982d763d64c0f6e880ea6e0105df18ce5c20f6f5
Tristan Hume <tris.hume@gmail.com>
2015-01-09 16:40:50 -0500

    Basic crappy sequencing of 'in the jungle'

--- screentunes.js
+++ screentunes.js
@@ -3,6 +3,9 @@
   var ctx;
   var start = null;
   var t = null;
+  var factor = 10;
+  var song = [1,2,3,2,3,4,3,2,1,2,3,2,1,3,2,0,0];
+  var notes = [0,2093,2349,2637,2794,3136,3520,3729,3951,4186];
 
   var AnimationFrame = (function() {
     var FPS = 16.6666666667; // 1000 / 60 = Frames Per Second
@@ -52,8 +55,8 @@
 
   function render() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var magic = 5 + (Math.sin(t / 2000)+1)*7;
-    bars(magic, magic);
+    // var magic = 5 + (Math.sin(t / 2000)+1)*7;
+    bars(factor, factor);
   }
 
   function frame(timestamp) {
@@ -63,11 +66,27 @@
     AnimationFrame.request(frame);
   }
 
+  function playFreq(f) {
+    var maxf = 60*screen.height/2; // methinks this is the frequency a 1px on 1px off cycle would produce in theory
+    factor = maxf/f;
+  }
+
+  function playSong(n) {
+    playFreq(notes[song[n]]);
+    setTimeout(function() {rest((n+1) % song.length);},400);
+  }
+
+  function rest(n) {
+    playFreq(0);
+    setTimeout(function() {playSong(n);},40);
+  }
+
   $(document).ready(function() {
     canvas = document.getElementById("main");
     ctx = canvas.getContext("2d");
     $(window).bind("resize", resize);
     resize();
+    playSong(0);
     AnimationFrame.request(frame);
   });
 })();
